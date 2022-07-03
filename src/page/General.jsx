4397522cbf669d7a0ce36d95447a4b30f5a5e2fd
hoothin <rixixi@gmail.com>
2022-07-03 20:34:48 +0800

    Update General.jsx

--- src/page/General.jsx
+++ src/page/General.jsx
@@ -89,7 +89,7 @@ export default function General() {
                     <h4>{window.i18n('toolbarPosition')}</h4>
                 </Typography>
                 <Box
-                  sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', maxWidth: '100%', flexWrap: 'wrap' }}
+                  sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', maxWidth: '100%', flexWrap: 'wrap', justifyContent: 'space-evenly' }}
                 >
                     <FormControl sx={{ m: 1, minWidth: 80 }}>
                         <InputLabel id="demo-simple-select-autowidth-label">{window.i18n('horizontal')}</InputLabel>
