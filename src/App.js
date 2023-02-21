3b9118fd025814d7b603e7f9e0230a8c6f4c5b77
hoothin <rixixi@gmail.com>
2023-02-21 18:46:07 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import FindInPage from './page/FindInPage.jsx';
 import Link from '@mui/material/Link';
 import { createClient } from "webdav";
 
-const version = "1.6.6.54.23";
+const version = "1.6.6.55.1";
 
 interface TabPanelProps {
   children?: React.ReactNode;
