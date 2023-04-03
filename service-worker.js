27b2d13b226c9a7fdd864f3e6087764827e8e867
hoothin <rixixi@gmail.com>
2023-04-03 09:33:45 +0000

    Deploying to gh-pages from @ hoothin/SearchJumper@26d06999d0bdae69a05051fa69ef13ed944830d1 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.df1956df84d56c7ceedf8a925ca2dd1b.js"
+  "./precache-manifest.a9e1bb7adf099e3954fbce4d7018906f.js"
 );
 
 workbox.clientsClaim();
