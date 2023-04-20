92cb9d038adde65d1f24fa3f6b3be1a9a39c5002
hoothin <rixixi@gmail.com>
2023-04-20 15:39:37 +0800

    Update Engines.jsx

--- src/page/Engines.jsx
+++ src/page/Engines.jsx
@@ -1487,7 +1487,8 @@ export default function Engines() {
       createData('%date{}', window.i18n('param_date2')),
       createData('%element{}.prop()', window.i18n('param_elep')),
       createData('%element{}.replace()', window.i18n('param_elere')),
-      createData('copy:', window.i18n('param_cp'))
+      createData('copy:', window.i18n('param_cp')),
+      createData('showTips:', window.i18n('param_showTips'))
     ];
     let selectTxt = -1, selectImg = -1, selectLink = -1, selectPage = -1, selectAll = -1;
     for (let i = 0; i < window.searchData.sitesConfig.length; i++) {
